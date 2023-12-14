5228172293ae53024e849d00144c84021cfad2ee
Michael_Fesenko <@gmail.com>
2023-12-14 16:08:56 +0300

    add HW_Sem_07_2

---
+++ HW_Sem_07_2/Main.java
@@ -0,0 +1,11 @@
+package HW_Sem_07_2;
+
+import HW_Sem_07_2.controller.CalculatorController;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorController calculatorController = new CalculatorController();
+        calculatorController.start();
+    }
+
+}
