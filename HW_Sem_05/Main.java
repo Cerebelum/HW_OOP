53e6fcc063736830e3e8f23ab9fb5d021d52cc31
Michael_Fesenko <@gmail.com>
2023-12-14 08:30:29 +0300

    fix

--- HW_Sem_05/Main.java
+++ HW_Sem_05/Main.java
@@ -24,49 +24,18 @@ public class Main {
         // controller.createTeacher("Александр", "Андреевич", "Пузиков");
         // controller.printAllTeachers();
 
-// *******************************************************************************************************
+// ************************************ Домашки ****************************************
         Controller controller = new Controller();
         controller.createStudent("Иван", "Иванович", "Иванов");
         controller.createStudent("Петр", "Петрович", "Петров");
         controller.createStudent("Сидор", "Сидорович", "Сидоров");
-        // System.out.println(controller.getAllStudents());
-        // controller.printAllStudents();
-        
 
         controller.createTeacher("Марфа", "Васильевна", "Сорокопят");
-        // System.out.println(controller.getAllTeachers());
 
         StudyGroupService studyGroupService = new StudyGroupService();
         StudyGroup studyGroup1 = studyGroupService.createStudyGroup(controller.getTeacherByID(0), controller.getAllStudents());
         System.out.println(studyGroup1);
 
-
-
-
-// ************************************ домашка *****************************************
-        // Создаем студентов
-        // Student student1 = new Student("Иван", "Иванович", "Иванов", 123);
-        // Student student2 = new Student("Петр", "Петрович", "Петров", 456);
-        // Student student3 = new Student("Сидор", "Сидорович", "Сидоров", 789);
-
-        // Создаем преподавателя
-        // Teacher teacher = new Teacher("Алексей", "Алексеевич", "Алексеев", 777);
-
-        // Создаем список студентов
-        // List<Student> students = new ArrayList<>();
-        // students.add(student1);
-        // students.add(student2);
-        // students.add(student3);
-
-        // Создаем объект StudyGroupService
-        // StudyGroupService studyGroupService = new StudyGroupService();
-
-        // Создаем учебную группу
-        // StudyGroup studyGroup = studyGroupService.createStudyGroup(teacher, students);
-
-        // Выводим учебную группу в консоль
-        // System.out.println(studyGroup);
     }
-
 
 }
